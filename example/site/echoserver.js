172ee3563ffa0b1c40dc8bae2560406768762579
Bill St. Clair <billstclair@gmail.com>
2018-09-03 00:11:13 -0400

    echoserver.js actually listens on its advertised port.

--- example/site/echoserver.js
+++ example/site/echoserver.js
@@ -15,13 +15,15 @@ if (process.argv[2]) {
 }
 
 var WSServer = require('ws').Server
-var wss      = new WSServer({host: "localhost", port: 8888})
+var wss      = new WSServer({port: port})
 
 console.log ("Listening on ws://localhost:" + port);
   
 // handle only pure clean websockets
 wss.on('connection', function(ws) {
+  console.log("connection");
   ws.on('message', function(message) {
+    console.log('sending: "' + message + '"');
     ws.send(message);
   })
 })
